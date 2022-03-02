c8abc18210bcf5829fae514ad5dabf1b206893d8
Yu Ding <dingelish@gmail.com>
2022-03-02 11:56:57 -0800

    fix: typo
    Signed-off-by: Yu Ding <dingelish@gmail.com>

--- sdk/trts/trts_veh.cpp
+++ sdk/trts/trts_veh.cpp
@@ -411,8 +411,8 @@ extern "C" sgx_status_t trts_handle_exception(void *tcs, outside_exitinfo_t *u_o
         g_enclave_state = ENCLAVE_CRASHED;
         return SGX_ERROR_STACK_OVERRUN;
     }
-
-    // sp is within limit_addr and commit_addr, currently only SGX 2.0 under hardware mode will enter this branch.^M
+    
+    // sp is within limit_addr and commit_addr, currently only SGX 2.0 under hardware mode will enter this branch.
     if((size_t)sp < thread_data->stack_commit_addr)
     {
         int ret = -1;
